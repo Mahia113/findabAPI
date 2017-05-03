864b57947bdac402360f4549fac55c502d0be734
maickol12 <rocm960107@outlook.com>
2017-05-03 01:19:28 -0500

    Archivos listos solo faltaria agregar el archivo de conexion

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php';
+//require 'conection.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$c = new \Slim\Container();
+$c['errorHandler'] = function ($c) {
+    return function ($request, $response, $exception) use ($c) {
+      $error = array('error' => $exception->getMessage());
+      return $c['response']->withStatus(500)
+                             ->withHeader('Content-Type', 'application/json')
+                             ->write(json_encode($error));
+    };
+};
+
+$app = new \Slim\App($c);
+//require 'utils.php';
+
+
+$app->run();
+
+?>

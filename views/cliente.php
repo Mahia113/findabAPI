0fec1e0e64178e47db0ace596f6909db11e021ea
maickol12 <rocm960107@outlook.com>
2017-05-04 01:24:17 -0500

    update

--- views/cliente.php
+++ views/cliente.php
@@ -12,5 +12,30 @@
     $cliente = Cliente::findOrFail($idCliente);
     sendResponse($cliente->toJson(),$res,200);
   });
+  $app->post('/clientes',function(Request $req,Response $res,$args){
+      $data = $req->getParsedBody();
+      $cliente = Cliente::where('correo',$data['correo'])->get();
+      if($cliente){
+        return sendResponse(json_encode(array('message'=>'Ya existe en la base de datos')),$res,500);
+      }
+      $cliente = new Cliente();
+      $cliente->nombre = $data['nombre'];
+      $cliente->apellido_paterno = $data['apellido_paterno'];
+      $cliente->apellido_materno = $data['apellido_materno'];
+      $cliente->correo = $data['correo'];
+      $cliente->contrasena = $data['contrasena'];
+      $cliente->registro_facebook = $data['registro_facebook'];
+      $cliente->foto = $data['foto'];
+      $cliente->tipo = $data['tipo'];
+      $cliente->direccion = $data['direccion'];
+      $cliente->habilitado = 1;
+      $cliente->ciudad_id = $data['ciudad_id'];
+      $cliente->c_tipo_cliente_id = $data['c_tiempo_cliente_id'];
+      $cliente->c_genero_id = $data['c_genero_id'];
 
+      if($cliente->save()){
+        return sendResponse($cliente->toJson(),$res,200);
+      }
+      sendResponse(json_encode(array('message'=>'Ocurrio un error')),$res,500);
+  });
 ?>
